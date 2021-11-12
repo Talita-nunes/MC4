147749521987ba68990495679622b206e5c1d4f5
Talita Santos <contato.talitansantos@gmail.com>
2021-11-12 13:44:52 -0300

    Initial Commit

---
+++ MC4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MC4
+//
+//  Created by Talita Nunes dos Santos on 12/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MC4/MC4App.swift
@@ -0,0 +1,17 @@
+//
+//  MC4App.swift
+//  MC4
+//
+//  Created by Talita Nunes dos Santos on 12/11/21.
+//
+
+import SwiftUI
+
+@main
+struct MC4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
